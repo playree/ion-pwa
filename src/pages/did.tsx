5fbb239b3b51c62f0fe60dcc07ff91c06e8963e7
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2022-05-29 23:51:40 +0900

    DID詳細追加。

--- src/pages/did.tsx
+++ src/pages/did.tsx
@@ -1,14 +1,33 @@
 import * as React from 'react';
 import { Navigate } from 'react-router-dom';
-import { Typography, Container, Grid, TextField} from '@mui/material';
+import { Typography, Container, TextField, Accordion, AccordionSummary, AccordionDetails} from '@mui/material';
+import { 
+  ExpandMore as IconExpandMore
+} from '@mui/icons-material'
 import { useDidContext } from '../layout/sideMenuLayout';
+import { PrivateKeyTool } from '../helpers/didTools';
 
 export const PageDid = () => {
   const didContext = useDidContext();
+  const [privateKeys, setPrivateKeys] = React.useState({signing:'', recovery:'', update:''});
+
+  const setup = async () => {
+    if (didContext.didModel) {
+      setPrivateKeys({
+        signing: JSON.stringify(await PrivateKeyTool.load(didContext.didModel.signingKeyId), null, 2),
+        recovery: JSON.stringify(await PrivateKeyTool.load(PrivateKeyTool.RESERVE_ID.RECOVERY), null, 2),
+        update: JSON.stringify(await PrivateKeyTool.load(PrivateKeyTool.RESERVE_ID.UPDATE), null, 2)
+      });
+    };
+  };
+
+  React.useEffect(() => {
+    setup();
+  });
 
   if (!didContext.didModel) {
     return (<Navigate to="/" replace />);
-  }
+  };
 
   return (
     <>
@@ -16,19 +35,79 @@ export const PageDid = () => {
         <Typography variant='h5' sx={{marginBottom: '16px'}}>
           DID詳細
         </Typography>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
+        <Accordion>
+          <AccordionSummary expandIcon={<IconExpandMore />}>
+            <Typography>DID(Long)</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
             <TextField
               label='DID(Long)'
               fullWidth
               multiline
+              maxRows={8}
               value={didContext.didModel.didLong}
               InputProps={{
                 readOnly: true,
+                sx: {fontSize: '12px'}
               }}
             />
-          </Grid>
-        </Grid>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary expandIcon={<IconExpandMore />}>
+            <Typography>SigningPrivateKey</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <TextField
+              label='SigningPrivateKey'
+              fullWidth
+              multiline
+              maxRows={8}
+              value={privateKeys.signing}
+              InputProps={{
+                readOnly: true,
+                sx: {fontSize: '12px'}
+              }}
+            />
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary expandIcon={<IconExpandMore />}>
+            <Typography>RecoveryPrivateKey</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <TextField
+              label='RecoveryPrivateKey'
+              fullWidth
+              multiline
+              maxRows={8}
+              value={privateKeys.recovery}
+              InputProps={{
+                readOnly: true,
+                sx: {fontSize: '12px'}
+              }}
+            />
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary expandIcon={<IconExpandMore />}>
+            <Typography>UpdatePrivateKey</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <TextField
+              label='UpdatePrivateKey'
+              fullWidth
+              multiline
+              maxRows={8}
+              value={privateKeys.update}
+              InputProps={{
+                readOnly: true,
+                sx: {fontSize: '12px'}
+              }}
+            />
+          </AccordionDetails>
+        </Accordion>
+        
       </Container>
     </>
   );
